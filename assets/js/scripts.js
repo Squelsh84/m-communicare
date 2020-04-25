45d184d582a6c7a67703b92a3f332b9832e6cdc0
squelsh84 <mrmjwalsh@gmail.com>
2020-04-25 21:36:08 +0100

    Added text to services block. Changed color of why me section.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,4 @@
-/*!
-    * Start Bootstrap - Creative v6.0.1 (https://startbootstrap.com/themes/creative)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-creative/blob/master/LICENSE)
-    */
+
 (function ($) {
   "use strict"; // Start of use strict
 
@@ -43,22 +39,6 @@
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
-
-  // Magnific popup calls
-  $('#portfolio').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
-  });
 
 
 
